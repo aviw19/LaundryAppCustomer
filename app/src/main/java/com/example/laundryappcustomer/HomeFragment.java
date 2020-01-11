38e222be3a5439aa0b765e3f4a86213fa76b3532
Mayank Mittal <mk.mittal1999@gmail.com>
2020-01-11 16:20:30 +0530

    minor changes

--- app/src/main/java/com/example/laundryappcustomer/HomeFragment.java
+++ app/src/main/java/com/example/laundryappcustomer/HomeFragment.java
@@ -18,40 +18,36 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.gson.Gson;
 import org.jetbrains.annotations.NotNull;
 
-import java.security.Timestamp;
 import java.util.ArrayList;
-import java.util.Date;
 
 import static android.content.Context.MODE_PRIVATE;
 
 public class HomeFragment extends Fragment {
     private ImageView mRequestOrder;
-    private ImageView mRequestOrderandIron;
+    private ImageView mRequestOrderAndIron;
     private TextView mTextComment;
     private String OrderID;
-    private ArrayList<Order> OrderList = new ArrayList<>();
     private String mComment;
     private Requests mReq;
-    private RadioButton sixKgbutton;
+    private RadioButton sixKgButton;
     private RadioButton EightKgButton;
-    private RadioButton pickupanddrop;
-    private RadioButton onlydrop;
-    private RadioButton onlypickup;
-    private RadioButton noservice;
+    private RadioButton pickUpAndDrop;
+    private RadioButton onlyDrop;
+    private RadioButton onlyPickup;
+    private RadioButton noService;
     private String price;
     private String service;
-    private FirebaseDatabase firebaseDatabase ;
     private DatabaseReference table_user ;
     private DatabaseReference table_user2;
     @Nullable
     @Override
     public View onCreateView(@NotNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        firebaseDatabase = FirebaseDatabase.getInstance();
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
         table_user = firebaseDatabase.getReference("Requests").child(Common.merchantphone);
         table_user2 = firebaseDatabase.getReference("Customer").child(Common.currentUser.getPhoneno());
        View rootView =inflater.inflate(R.layout.fragment_home, container, false);
        mRequestOrder=rootView.findViewById(R.id.request_Order);
-       mRequestOrderandIron = rootView.findViewById(R.id.request_Orderandiron);
+       mRequestOrderAndIron = rootView.findViewById(R.id.request_Orderandiron);
         FirebaseTokeGeneration.main();
 
         String token=FirebaseTokeGeneration.token;
@@ -69,12 +65,12 @@ public class HomeFragment extends Fragment {
                 LayoutInflater inflater = getLayoutInflater();
                 final View alertLayout = inflater.inflate(R.layout.request_comments1, null);
                 mTextComment=alertLayout.findViewById(R.id.comments);
-                sixKgbutton = alertLayout.findViewById(R.id.sixkgradiobutton1);
+                sixKgButton = alertLayout.findViewById(R.id.sixkgradiobutton1);
                 EightKgButton = alertLayout.findViewById(R.id.eightkgradiobutton1);
-                pickupanddrop = alertLayout.findViewById(R.id.pickupdrop1);
-                onlypickup = alertLayout.findViewById(R.id.pickupdrop2);
-                onlydrop=alertLayout.findViewById(R.id.pickupdrop3);
-                noservice=alertLayout.findViewById(R.id.pickupdrop4);
+                pickUpAndDrop = alertLayout.findViewById(R.id.pickupdrop1);
+                onlyPickup = alertLayout.findViewById(R.id.pickupdrop2);
+                onlyDrop =alertLayout.findViewById(R.id.pickupdrop3);
+                noService =alertLayout.findViewById(R.id.pickupdrop4);
                 alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
@@ -88,11 +84,11 @@ public class HomeFragment extends Fragment {
                         OrderID= Common.currentUser.getPhoneno()+Common.currentUser.getOrderCount();
                         mComment=mTextComment.getText().toString();
 
-                        if(sixKgbutton.isChecked())
+                        if(sixKgButton.isChecked())
                         {
                             weight="6KG";
                             price="199";
-                            sixKgbutton.setText("COST : 199RS");
+                            sixKgButton.setText("COST : 199RS");
                         }
                         if(EightKgButton.isChecked())
                         {
@@ -100,33 +96,33 @@ public class HomeFragment extends Fragment {
                             weight="8KG";
                             price="266";
                         }
-                        if(!(sixKgbutton.isChecked())&&!(EightKgButton.isChecked()))
+                        if(!(sixKgButton.isChecked())&&!(EightKgButton.isChecked()))
                         {
                             Toast.makeText(getActivity(),"Please select a weight slab",Toast.LENGTH_SHORT).show();
                         }
 
-                        if(pickupanddrop.isChecked())
+                        if(pickUpAndDrop.isChecked())
                         {
                             price=Integer.toString(20+Integer.parseInt(price));
                             service = "PICKUP AND DROP";
                             makingRequest1(weight,service);
                             addingOrder(weight,service);
                         }
-                        else if(onlypickup.isChecked())
+                        else if(onlyPickup.isChecked())
                         {
                             price=Integer.toString(10+Integer.parseInt(price));
                             service = "ONLY PICKUP FROM HOME";
                             makingRequest1(weight,service);
                             addingOrder(weight,service);
                         }
-                        else if(onlydrop.isChecked())
+                        else if(onlyDrop.isChecked())
                         {
                             price=Integer.toString(10+Integer.parseInt(price));
                             service = "ONLY DROP AT HOME";
                             makingRequest1(weight,service);
                             addingOrder(weight,service);
                         }
-                        else if(noservice.isChecked())
+                        else if(noService.isChecked())
                         {
                             price=Integer.toString(10+Integer.parseInt(price));
                             service = "NO PICKUP AND DROP";
@@ -147,7 +143,7 @@ public class HomeFragment extends Fragment {
             }
         });
 
-        mRequestOrderandIron.setOnClickListener(new View.OnClickListener() {
+        mRequestOrderAndIron.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
@@ -167,18 +163,18 @@ public class HomeFragment extends Fragment {
                         String weight="0KG";
                         OrderID= Common.currentUser.getPhoneno()+Common.currentUser.getOrderCount();
                         mComment=mTextComment.getText().toString();
-                        sixKgbutton = alertLayout.findViewById(R.id.sixkgradiobutton1);
+                        sixKgButton = alertLayout.findViewById(R.id.sixkgradiobutton1);
                         EightKgButton = alertLayout.findViewById(R.id.eightkgradiobutton1);
-                        pickupanddrop = alertLayout.findViewById(R.id.pickupdrop1);
-                        onlypickup = alertLayout.findViewById(R.id.pickupdrop2);
-                        onlydrop=alertLayout.findViewById(R.id.pickupdrop3);
-                        noservice=alertLayout.findViewById(R.id.pickupdrop4);
+                        pickUpAndDrop = alertLayout.findViewById(R.id.pickupdrop1);
+                        onlyPickup = alertLayout.findViewById(R.id.pickupdrop2);
+                        onlyDrop =alertLayout.findViewById(R.id.pickupdrop3);
+                        noService =alertLayout.findViewById(R.id.pickupdrop4);
 
-                        if(!sixKgbutton.isChecked() && !EightKgButton.isChecked())
+                        if(!sixKgButton.isChecked() && !EightKgButton.isChecked())
                         {
                             Toast.makeText(getActivity(),"Please select a weight slab",Toast.LENGTH_SHORT).show();
                         }
-                        if(sixKgbutton.isChecked())
+                        if(sixKgButton.isChecked())
                         {
                             weight="6KG";
                             price="245";
@@ -190,28 +186,28 @@ public class HomeFragment extends Fragment {
                         }
 
 
-                        if(pickupanddrop.isChecked())
+                        if(pickUpAndDrop.isChecked())
                         {
                             price=Integer.toString(20+Integer.parseInt(price));
                             service = "PICKUP AND DROP";
                             makingRequest2(weight,service);
                             addingOrder(weight,service);
                         }
-                        else if(onlypickup.isChecked())
+                        else if(onlyPickup.isChecked())
                         {
                             price=Integer.toString(10+Integer.parseInt(price));
                             service = "ONLY PICKUP FROM HOME";
                             makingRequest2(weight,service);
                             addingOrder(weight,service);
                         }
-                        else if(onlydrop.isChecked())
+                        else if(onlyDrop.isChecked())
                         {
                             price=Integer.toString(10+Integer.parseInt(price));
                             service = "ONLY DROP AT HOME";
                             makingRequest2(weight,service);
                             addingOrder(weight,service);
                         }
-                        else if(noservice.isChecked())
+                        else if(noService.isChecked())
                         {
                             price=Integer.toString(10+Integer.parseInt(price));
                             service = "NO PICKUP AND DROP";
@@ -243,6 +239,7 @@ public class HomeFragment extends Fragment {
         table_user2.child("firebaseToken").setValue(token);
 
     }
+
     private void makingRequest2(String weight,String service) {
         Long tsLong = System.currentTimeMillis()/1000;
         String ts = tsLong.toString();
@@ -252,7 +249,7 @@ public class HomeFragment extends Fragment {
         table_user2.child("firebaseToken").setValue(token);
     }
 
-            private void addingOrder(String weight,String service) {
+    private void addingOrder(String weight,String service) {
                 updatingOrderNo();
                 updateOrderList(weight,service);
                 changeInUser();
@@ -260,7 +257,7 @@ public class HomeFragment extends Fragment {
                 table_user2.child("firebaseToken").setValue(token);
             }
 
-            private void updatingOrderNo() {
+    private void updatingOrderNo() {
                 int count = Integer.parseInt(Common.currentUser.getOrderCount());
                 count++;
                 Common.currentUser.setOrderCount(String.valueOf(count));
@@ -270,20 +267,20 @@ public class HomeFragment extends Fragment {
         Long tsLong = System.currentTimeMillis();
         String ts = tsLong.toString();
         Order n = new Order(mComment, "REQUESTED", weight, OrderID,price,mReq.getService(),"NOT PAID",Common.merchantphone,ts);
-        OrderList = Common.currentUser.getOrderList();
-        if (OrderList != null) {
-            OrderList.add(n);
-            Common.currentUser.setOrderList(OrderList);
+        ArrayList<Order> orderList = Common.currentUser.getOrderList();
+        if (orderList != null) {
+            orderList.add(n);
+            Common.currentUser.setOrderList(orderList);
         }
         else
         {
-            OrderList = new ArrayList<>();
-            OrderList.add(n);
-            Common.currentUser.setOrderList(OrderList);
+            orderList = new ArrayList<>();
+            orderList.add(n);
+            Common.currentUser.setOrderList(orderList);
         }
     }
 
-            private void changeInUser() {
+    private void changeInUser() {
                 table_user2.setValue(Common.currentUser);
                 SharedPreferences sharedPreferences= this.getActivity().getSharedPreferences(Common.SHARED_PREFS, MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPreferences.edit();
