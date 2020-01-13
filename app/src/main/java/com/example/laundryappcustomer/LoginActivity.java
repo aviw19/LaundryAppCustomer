a22e38733231e36a223294abcdecfe752978da76
AVI WADHWA <rsavi0512@gmail.com>
2020-01-14 00:22:10 +0530

    finally done

--- app/src/main/java/com/example/laundryappcustomer/LoginActivity.java
+++ app/src/main/java/com/example/laundryappcustomer/LoginActivity.java
@@ -2,9 +2,13 @@ package com.example.laundryappcustomer;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.ContactsContract;
@@ -17,32 +21,37 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.gson.Gson;
 
 public class LoginActivity extends AppCompatActivity {
+    int PERMISSION_ALL=1;
+    private String[] PERMISSIONS = {Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_SMS};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        while(!hasPermissions(LoginActivity.this, PERMISSIONS)) {
+            ActivityCompat.requestPermissions(LoginActivity.this, PERMISSIONS, PERMISSION_ALL);
+        }
         if (loadData()) {
             new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
+                    @Override
+                    public void run() {
 
-                    Intent i = new Intent(LoginActivity.this, HomeActivity.class);
-                    startActivity(i);
-                    finish();
-                }
-            }, 350);
-        } else {
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
+                        Intent i = new Intent(LoginActivity.this, HomeActivity.class);
+                        startActivity(i);
+                        finish();
+                    }
+                }, 350);
+            } else {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
 
-                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(i);
-                    finish();
-                }
-            }, 350);
+                        Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(i);
+                        finish();
+                    }
+                }, 350);
 
-        }
+            }
     }
         public boolean loadData ()
         {
@@ -55,4 +64,14 @@ public class LoginActivity extends AppCompatActivity {
             state = sharedPreferences.getBoolean(Common.LOGIN, false);
             return state;
         }
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
