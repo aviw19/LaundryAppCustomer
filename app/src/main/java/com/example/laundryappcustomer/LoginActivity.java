5e016e395bb2fa9dcf935d081038ef1c3c93b223
AVI WADHWA <rsavi0512@gmail.com>
2020-01-17 13:30:36 +0530

    launch

--- app/src/main/java/com/example/laundryappcustomer/LoginActivity.java
+++ app/src/main/java/com/example/laundryappcustomer/LoginActivity.java
@@ -10,9 +10,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.ContactsContract;
+import android.provider.Settings;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -24,12 +27,14 @@ import com.google.gson.Gson;
 public class LoginActivity extends AppCompatActivity {
     private static final int MY_PERMISSIONS_REQUEST_INTERNET = 0;
     private static final int MY_PERMISSIONS_REQUEST_RECIEVE_SMS = 1;
-    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 2;
+    //private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 2;
+    private static final int MY_PERMISSIONS_REQUEST_CODE = 123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        if (ContextCompat.checkSelfPermission(LoginActivity.this,
+        checkPermissionM();
+        /*if (ContextCompat.checkSelfPermission(LoginActivity.this,
                 Manifest.permission.RECEIVE_SMS)
                 != PackageManager.PERMISSION_GRANTED ) {
 
@@ -100,36 +105,59 @@ public class LoginActivity extends AppCompatActivity {
             }
         } else {
             // Permission has already been granted
-        }
-        if (loadData())
-        {
+        }*/
 
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
+    }
 
-                    Intent i=new Intent(LoginActivity.this,HomeActivity.class);
-                    startActivity(i);
-                    finish();
-                }
-            }, 350);
+    private void checkPermissionM() {
+        if(ContextCompat.checkSelfPermission(LoginActivity.this,Manifest.permission.RECEIVE_SMS)
+                + ContextCompat.checkSelfPermission(
+                LoginActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
 
+            ActivityCompat.requestPermissions(
+                    LoginActivity.this,
+                    new String[]{
+                            Manifest.permission.RECEIVE_SMS,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    },
+                    MY_PERMISSIONS_REQUEST_CODE
+            );
         }
         else
         {
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
+            if (loadData())
+            {
 
-                    Intent i=new Intent(LoginActivity.this,MainActivity.class);
-                    startActivity(i);
-                    finish();
-                }
-            }, 350);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
 
+                        Intent i=new Intent(LoginActivity.this,HomeActivity.class);
+                        startActivity(i);
+                        finish();
+                    }
+                }, 350);
+
+            }
+            else
+            {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        Intent i=new Intent(LoginActivity.this,MainActivity.class);
+                        startActivity(i);
+                        finish();
+                    }
+                }, 350);
+
+            }
         }
+
     }
-        public boolean loadData ()
+
+    public boolean loadData ()
         {
             Boolean state;
             SharedPreferences sharedPreferences = getSharedPreferences(Common.SHARED_PREFS, MODE_PRIVATE);
@@ -145,13 +173,62 @@ public class LoginActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode,
                                            String[] permissions, int[] grantResults) {
         switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE: {
+            case MY_PERMISSIONS_REQUEST_CODE: {
                 // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if ((grantResults.length) > 0
+                        && (grantResults[0] +grantResults[1])== PackageManager.PERMISSION_GRANTED) {
                     // permission was granted, yay! Do the
                     // contacts-related task you need to do.
+                    if (loadData())
+                    {
+
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+
+                                Intent i=new Intent(LoginActivity.this,HomeActivity.class);
+                                startActivity(i);
+                                finish();
+                            }
+                        }, 350);
+
+                    }
+            else
+                    {
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+
+                                Intent i=new Intent(LoginActivity.this,MainActivity.class);
+                                startActivity(i);
+                                finish();
+                            }
+                        }, 350);
+
+                    }
                 } else {
+                    for (int i = 0, len = permissions.length; i < len; i++) {
+                        String permission = permissions[i];
+                        if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                            boolean showRationale = shouldShowRequestPermissionRationale(permission);
+                            if (!showRationale) {
+                                Toast.makeText(LoginActivity.this,"Please Grant All Permissions",Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                Uri uri = Uri.fromParts("package", getPackageName(), null);
+                                intent.setData(uri);
+                                startActivityForResult(intent, MY_PERMISSIONS_REQUEST_CODE);
+                            } else {
+                                Toast.makeText(LoginActivity.this,"Please Grant All Permissions",Toast.LENGTH_SHORT).show();
+                                checkPermissionM();
+
+                                // user did NOT check "never ask again"
+                                // this is a good place to explain the user
+                                // why you need the permission and ask if he wants
+                                // to accept it (the rationale)
+                            }
+                        }
+                    }
+
                     // permission denied, boo! Disable the
                     // functionality that depends on this permission.
                 }
